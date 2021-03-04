4b9dc3539ed341e5ad5ec501133611c8a0655fb3
P0NDER0SA <mikepond11@gmail.com>
2021-03-04 15:34:29 -0500

    fixing dependencies

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/DTS-STN/openfisca-canada-dts-csps',
     include_package_data=True,  # Will read MANIFEST.in
     install_requires=[
-        'OpenFisca-Core == 35.1.0',
+        'OpenFisca-Core >=30.0.0,<31.0',
         ],
     extras_require={
         'test': [
