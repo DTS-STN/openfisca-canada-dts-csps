3fb2b078b2a8b0de130ba96f29354bee4dfcf7b6
Thomas Dacey <DaceyTom2@gmail.com>
2021-03-15 11:11:46 -0400

    code changes for live demo

---
+++ openfisca_canada_mvohwr/variables/employment/does_transport_goods_or_passangers_by_motor_vehicle.py
@@ -0,0 +1,9 @@
+from openfisca_core.model_api import *
+from openfisca_canada_mvohwr.entities import Person
+
+class employment__does_transport_goods_or_passangers_by_motor_vehicle(Variable):
+    value_type = bool
+    entity = Person
+    label = u"TODO"
+    definition_period = DAY
+    reference = u"TODO"
