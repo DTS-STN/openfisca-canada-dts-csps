0c134c3398bb3349037f24ab673f47a8c867c98d
Thomas Dacey <DaceyTom2@gmail.com>
2021-03-16 15:36:50 -0400

    holiday standard hours cannot go below zero

--- openfisca_canada_mvohwr/variables/motor_vehicle_operator_hours_of_work_regulation/mvo__standard_hours_of_work.py
+++ openfisca_canada_mvohwr/variables/motor_vehicle_operator_hours_of_work_regulation/mvo__standard_hours_of_work.py
@@ -11,6 +11,7 @@ class standard_hours__weekly(Variable):
         if (persons("weekly_work_schedule__has_holiday_days_in_period",period)):
             daily_hours = parameters(period).clc_standard_hours_of_work.daily_clc_standard_hours_of_work
             number_of_holidays = persons("weekly_work_schedule__total_holiday_days_in_period",period)
+            clc_holiday_adjusted = parameters(period).clc_standard_hours_of_work.weekly_clc_standard_hours_of_work - (daily_hours * number_of_holidays)
             return select(
                 [
                     persons("standard_hours__is_weekly_clc_standard_hours_of_work",period),
@@ -18,7 +19,7 @@ class standard_hours__weekly(Variable):
                     persons("standard_hours__is_hmvo_weekly_mvo_hours_of_work",period)
                 ],
                 [
-                    parameters(period).clc_standard_hours_of_work.weekly_clc_standard_hours_of_work - (daily_hours * number_of_holidays),
+                    where(clc_holiday_adjusted < 0, 0, clc_holiday_adjusted),
                     parameters(period).mvo_standard_hours_of_work.cmvo_weekly_holiday_mvo_standard_hours_of_work,
                     parameters(period).mvo_standard_hours_of_work.hmvo_weekly_holiday_mvo_standard_hours_of_work
                     ],
