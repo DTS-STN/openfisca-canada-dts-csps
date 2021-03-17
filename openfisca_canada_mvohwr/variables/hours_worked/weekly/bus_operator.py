02ced11ff7154b7fde486ace80db535e442df07b
Thomas Dacey <DaceyTom2@gmail.com>
2021-03-17 13:28:49 -0400

    removed duplicate classes

--- openfisca_canada_mvohwr/variables/hours_worked/weekly/bus_operator.py
+++
@@ -1,9 +0,0 @@
-from openfisca_core.model_api import *
-from openfisca_canada_mvohwr.entities import Person
-
-class hours_worked__weekly__bus_operator(Variable):
-    value_type = float
-    entity = Person
-    label = u"Placeholder"
-    definition_period = DAY
-    reference = u"TODO"
