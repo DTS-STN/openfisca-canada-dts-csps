b1902d3d3277352acd79d961992b4d02b208a0c4
Thomas Dacey <DaceyTom2@gmail.com>
2021-03-18 10:18:09 -0400

    standard hours split into clc cmvo hmvo

--- openfisca_canada_mvohwr/variables/standard_hours/standard_hours_weekly.py
+++ openfisca_canada_mvohwr/variables/standard_hours/standard_hours_weekly.py
@@ -1,47 +1,64 @@
 from openfisca_core.model_api import *
 from openfisca_canada_mvohwr.entities import Person
 
-class standard_hours__weekly(Variable):
+
+class standard_hours__weekly_clc(Variable):
     value_type = float
     entity = Person
     label = u"Placeholder"
     definition_period = DAY
     reference = u"TODO"
+    
     def formula(persons, period, parameters):
-        if (persons("weekly_work_schedule__has_holiday_days_in_period",period)):
-            daily_hours = parameters(period).clc_standard_hours_of_work.daily_clc_standard_hours_of_work
-            number_of_holidays = persons("weekly_work_schedule__total_holiday_days_in_period",period)
-            clc_holiday_adjusted = parameters(period).clc_standard_hours_of_work.weekly_clc_standard_hours_of_work - (daily_hours * number_of_holidays)
-            return select(
-                [
-                    persons("standard_hours__is_weekly_clc_standard_hours_of_work",period),
-                    persons("standard_hours__is_cmvo_weekly_mvo_standard_hours_of_work",period),
-                    persons("standard_hours__is_hmvo_weekly_mvo_hours_of_work",period)
-                ],
-                [
-                    where(clc_holiday_adjusted < 0, 0, clc_holiday_adjusted),
-                    parameters(period).mvo_standard_hours_of_work.cmvo_weekly_holiday_mvo_standard_hours_of_work,
-                    parameters(period).mvo_standard_hours_of_work.hmvo_weekly_holiday_mvo_standard_hours_of_work
-                    ],
-                    parameters(period).clc_standard_hours_of_work.weekly_clc_standard_hours_of_work - (daily_hours * number_of_holidays)
-            )
-        else:
-            return select(
-                [
-                    persons("standard_hours__is_weekly_clc_standard_hours_of_work",period),
-                    persons("standard_hours__is_cmvo_weekly_mvo_standard_hours_of_work",period),
-                    persons("standard_hours__has_autherized_exemption",period),
-                    persons("standard_hours__is_hmvo_weekly_mvo_hours_of_work",period)
-                ],
-                [
-                    parameters(period).clc_standard_hours_of_work.weekly_clc_standard_hours_of_work,
-                    parameters(period).mvo_standard_hours_of_work.cmvo_weekly_mvo_standard_hours_of_work,
-                    persons("standard_hours__weekly_alternative",period),
-                    parameters(period).mvo_standard_hours_of_work.hmvo_weekly_mvo_hours_of_work
-                ],
-                parameters(period).clc_standard_hours_of_work.weekly_clc_standard_hours_of_work
-            )
+        clc_weekly_hours = parameters(period).clc_standard_hours_of_work.weekly_clc_standard_hours_of_work
+        clc_daily_hours = parameters(period).clc_standard_hours_of_work.daily_clc_standard_hours_of_work
+        
+        nb_holidays = persons("weekly_work_schedule__total_holiday_days_in_period",period)
+        clc_holiday_adjusted_hours = clc_weekly_hours - (clc_daily_hours * nb_holidays)
+        
+        return where(clc_holiday_adjusted_hours < 0, 0, clc_holiday_adjusted_hours)
 
+class standard_hours__weekly_cmvo(Variable):
+    value_type = float
+    entity = Person
+    label = u"Placeholder"
+    definition_period = DAY
+    reference = u"TODO"
+
+    def formula(persons, period, parameters):
+        cmvo_weekly_hours = parameters(period).mvo_standard_hours_of_work.cmvo_weekly_mvo_standard_hours_of_work
+        cmvo_daily_hours = parameters(period).mvo_standard_hours_of_work.cmvo_daily_mvo_standard_hours_of_work
+
+        nb_holidays = persons("weekly_work_schedule__total_holiday_days_in_period",period)
+        adjusted_holiday_hours = cmvo_weekly_hours - (cmvo_daily_hours * nb_holidays)
+
+        return where(adjusted_holiday_hours < 0, 0, adjusted_holiday_hours)
+
+
+class standard_hours__weekly_hmvo(Variable):
+    value_type = float
+    entity = Person
+    label = u"Placeholder"
+    definition_period = DAY
+    reference = u"TODO"
+
+    def formula(persons, period, parameters):
+        hmvo_weekly_hours = parameters(period).mvo_standard_hours_of_work.hmvo_weekly_mvo_hours_of_work
+        hmvo_daily_hours = 10 # maybe? does anyone really know?
+
+        nb_holidays = persons("weekly_work_schedule__total_holiday_days_in_period",period)
+        adjusted_holiday_hours = hmvo_weekly_hours - (hmvo_daily_hours * nb_holidays)
+
+        return select(
+                [
+                    persons("standard_hours__has_autherized_exemption",period),
+                    not_(persons("standard_hours__has_autherized_exemption",period))
+                ],
+                [
+                    persons("standard_hours__weekly_alternative",period),
+                    where(adjusted_holiday_hours < 0, 0, adjusted_holiday_hours)
+                ]
+            )
 
 class standard_hours__daily(Variable):
     value_type = float
