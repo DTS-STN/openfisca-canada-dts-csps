b6eb953f2c783433c77222348c6b1804fd596be8
Thomas Dacey <DaceyTom2@gmail.com>
2021-03-23 14:15:16 -0400

    max_ only accepts 2 arguements

--- openfisca_canada_mvohwr/variables/calculate_overtime/calculate_overtime_overall.py
+++ openfisca_canada_mvohwr/variables/calculate_overtime/calculate_overtime_overall.py
@@ -9,8 +9,11 @@ class calculate_overtime__overtime_worked_hours(Variable):
     reference = u"https://laws-lois.justice.gc.ca/eng/regulations/C.R.C.,_c._990/page-1.html"
 
     def formula(persons, period, parameters):
-      #take the better of daily vs weekly
       daily_ot = persons("calculate_overtime_daily__overtime_worked_hours",period)
       weekly_ot = persons("calculate_overtime_weekly__overtime_worked_hours",period)
       sixty_hour_ot = persons("calculate_overtime_weekly__hmvo_overtime_worked_hours", period)
-      return max_(daily_ot, weekly_ot, sixty_hour_ot)
+      #take the better of daily vs weekly
+      ot_worked_hours = max_(daily_ot, weekly_ot)
+      #take 60h if greater
+      ot_worked_hours = max_(ot_worked_hours, sixty_hour_ot)
+      return ot_worked_hours
