ab5f809932285068e9e919e95a44a0c2111d9ba2
Thomas Dacey <DaceyTom2@gmail.com>
2021-03-23 15:58:55 -0400

    check for zero and tests

--- openfisca_canada_mvohwr/variables/calculate_overtime/calculate_overtime_averaged.py
+++ openfisca_canada_mvohwr/variables/calculate_overtime/calculate_overtime_averaged.py
@@ -9,7 +9,7 @@ class calculate_overtime__averaged(Variable):
     reference = u"https://laws-lois.justice.gc.ca/eng/regulations/C.R.C.,_c._990/page-1.html"
 
     def formula(persons, period, parameters):
-      ot_hours = persons("calculate_overtime__total_overtime_clc_hours", period) / persons("calculate_overtime__number_of_averaging_scheduled_clc_weeks", period)
+      ot_hours = where(persons("calculate_overtime__number_of_averaging_scheduled_clc_weeks", period)==0, 0, persons("calculate_overtime__total_overtime_clc_hours", period) / persons("calculate_overtime__number_of_averaging_scheduled_clc_weeks", period))
       return max_(0, ot_hours)
 
 class calculate_overtime__total_overtime_clc_hours(Variable):
